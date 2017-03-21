b239e0e40cc31cac4f816811125fbb3641fa3e9a
Galia <galiat@gmail.com>
2017-03-21 15:29:46 -0400

    streaming api is closed right now! doing some shitty polling real quick.

--- bot.rb
+++ bot.rb
@@ -1,10 +1,33 @@
 require_relative 'nearby_finder'
 require 'slack-notifier'
 
-nearby_tweets = NearbyFinder.new.get_nearby
+class DryDockBot
+  attr_reader :tweet_urls_so_far
 
-notifier = Slack::Notifier.new(ENV["WEBHOOK_URL"])
+  def initialize
+    @notifier = Slack::Notifier.new(ENV["WEBHOOK_URL"])
+    @tweet_urls_so_far = []
+  end
 
-nearby_tweets.each{ | nearby_tweet | notifier.ping("nearby: #{nearby_tweet.url.to_s}") }
+  def run
+    while(true)
+      find_and_publish_nearby_mons
+      sleep 45
+    end
+  end
 
+  def find_and_publish_nearby_mons
+    nearby_tweets = NearbyFinder.new.get_nearby
+    nearby_tweet_urls = nearby_tweets.map{ |nearby_tweet| nearby_tweet.url.to_s}
 
+    new_nearby_urls = nearby_tweet_urls - @tweet_urls_so_far
+    @tweet_urls_so_far.concat new_nearby_urls
+    #new_nearby_urls.each{ | nearby | @notifier.ping("nearby: #{nearby_tweet}") }
+    new_nearby_urls.each{ | nearby | puts("nearby: #{nearby}") }
+  end
+end
+
+bot = DryDockBot.new
+bot.run
+
+bot.tweet_urls_so_far
